7085b9cb64deb96d6a756ae986f11986136546bd
Derek Paterson <athens_code@gmx.com>
2013-05-09 14:18:39 +0100

    Added some models and some associated test cases.

--- VilfredoReloadedCore/models.py
+++ VilfredoReloadedCore/models.py
@@ -3,7 +3,7 @@
 #
 # This file is part of VilfredoReloadedCore.
 #
-# Copyright (c) 2013 Daniele Pizzolli <daniele@ahref.eu>
+# Copyright (c) 2013 Derek Paterson <athens_code@gmx.com>
 #
 # VilfredoReloadedCore is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -21,30 +21,240 @@
 
 
 '''
-The database models
+The database Bases
 '''
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from sqlalchemy import Enum, DateTime, Text
+
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
+import datetime
 
-class LoginUser(Base):
+
+class Update(Base):
     '''
-    Stores the user mandatory data
+    Stores user question subscription data
+    '''
+
+    __tablename__ = 'update'
+
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    how = Column(Enum('daily', 'weekly', 'asap'))
+    last_update = Column(DateTime)
+    # M:1
+    question_id = Column(Integer, ForeignKey('question.id'), primary_key=True)
+    subscribed_to = relationship("Question", backref="subscriber_update")
+
+    def __init__(self, subscriber, subscribed_to, how=None):
+        self.subscriber = subscriber
+        self.subscribed_to = subscribed_to
+        self.how = how or 'asap'
+
+
+class User(Base):
+    '''
+    Stores the user data
     '''
 
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(255), unique=True, nullable=False)
-    email = Column(String(255), nullable=False)
-    password = Column(String(255), nullable=False)
+    username = Column(String(64), unique=True, nullable=False)
+    email = Column(String(120), unique=True)
+    password = Column(String(60))
+    registered = Column(DateTime)
+    last_seen = Column(DateTime)
+    # 1:M
+    questions = relationship('Question', backref='author', lazy='dynamic')
+    proposals = relationship('Proposal', backref='author', lazy='dynamic')
+    endorsements = relationship('Endorsement',
+                                backref='endorser', lazy='dynamic')
+    # updates 1:M
+    subscribed_questions = relationship("Update", backref='subscriber',
+                                        lazy='dynamic')
+    # invites M:M
+    invites = relationship("Invite", primaryjoin="User.id==Invite.sender_id",
+                           backref="sender", lazy='dynamic')
+
+    def invite(self, receiver, question_id):
+        self.invites.append(Invite(receiver, question_id))
+        return self
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
+        self.registered = datetime.datetime.utcnow()
+        self.last_seen = datetime.datetime.utcnow()
 
     def __repr__(self):
-        return "<UID: %d, User: %s>" % (self.id, self.username)
+        return "<Vilfredo User('%s','%s', '%s')>" % (self.username,
+                                                     self.email,
+                                                     self.password)
+
+
+class Invite(Base):
+    '''
+    Stores users invitaions to participate in questions
+    '''
+
+    __tablename__ = 'invite'
+
+    sender_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    receiver_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    question_id = Column(Integer, primary_key=True)
+
+    receiver = relationship("User", primaryjoin="Invite.receiver_id==User.id",
+                            backref="invitations", lazy='static')
+
+    def __init__(self, receiver, question_id):
+        self.receiver = receiver
+        self.question_id = question_id
+
+
+class Question(Base):
+    '''
+    Stores question data
+    '''
+
+    __tablename__ = 'question'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(120), nullable=False)
+    blurb = Column(Text, nullable=False)
+    generation = Column(Integer, default=1)
+    created = Column(DateTime)
+    room = Column(String(20))
+    phase = Column(Enum('writing', 'voting', 'archived'), default='writing')
+    last_move_on = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    # 1:M
+    proposals = relationship('Proposal', backref='question', lazy='dynamic')
+
+    def __init__(self, author, title, blurb, room=None):
+        self.author = author
+        self.title = title
+        self.blurb = blurb
+        self.room = room or ''
+        self.created = datetime.datetime.utcnow()
+
+    def change_phase(self, phase=None):
+        if (phase is None):
+            if (self.phase == 'writing'):
+                self.phase = 'voting'
+            else:
+                self.phase = 'writing'
+        else:
+            self.phase = phase
+        return self
+
+    def __repr__(self):
+        return "<User('%s','%s', '%s')>" % (self.title,
+                                            self.blurb,
+                                            self.author.username)
+
+
+class Proposal(Base):
+    '''
+    Represents the proposal object
+    '''
+
+    __tablename__ = 'proposal'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(120))
+    blurb = Column(Text, nullable=False)
+    generation = Column(Integer, default=1)
+    created = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    question_id = Column(Integer, ForeignKey('question.id'))
+    dominated_by = Column(Integer, default=0)
+    # 1:M
+    endorsements = relationship('Endorsement', backref="proposal",
+                                lazy='dynamic')
+
+    def __init__(self, author, question, title, blurb):
+        self.author = author
+        self.question = question
+        self.title = title
+        self.blurb = blurb
+        self.generation = 1
+        self.created = datetime.datetime.utcnow()
+
+    def endorse(self, endorser):
+        self.endorsements.append(Endorsement(endorser, self))
+        return self
+
+    def remove_endorsement(self, endorser):
+        pass
+
+    @staticmethod
+    def who_dominates_who(p1, p2):
+        '''
+        Takes 2 SETS of ENDORSER IDs, one for each proposal,
+        and calulates which of any domiantes the other.
+
+        Returns the dominating non-empty proposal set or ---
+        -1 if both sets are the same
+        0 if both sets are different
+        '''
+
+        # Check if p1 is empty
+        if (len(p1) > 0):
+            # Check if p2 is empty
+            if (len(p2) > 0):
+                # Check if p1 cpntains elements not in p2
+                if (len(p1.difference(p2)) > 0):
+                    # Check if p2 cpntains elements not in p1
+                    if (len(p2.difference(p1)) > 0):
+                        # Sets are different, neither dominates
+                        return 0
+                    else:
+                        # p2 is a subset of p1 so p1 dominates
+                        return p1
+                else:
+                    if (len(p2.difference(p1)) > 0):
+                        # p1 is a subset of p2 so p2 dominates
+                        return p2
+                    else:
+                        # Both are the same so neither dominates
+                        return -1
+            else:
+                # p2 is empty and p2 is non-empty so p2 dominates
+                return p1
+        elif (len(p2) > 0):
+            # p1 is empty and p2 is non-empty so p2 dominates
+            return p2
+        else:
+            # Both are empty so neither dominates
+            return -1
+
+    def __repr__(self):
+        return "<User('%s','%s', '%s', '%s')>" % (self.title,
+                                                  self.blurb,
+                                                  self.author.username,
+                                                  self.question_id)
+
+
+class Endorsement(Base):
+    '''
+    Stores endorsement data
+    '''
+
+    __tablename__ = 'endorsement'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    proposal_id = Column(Integer, ForeignKey('proposal.id'), nullable=False)
+    endorsement_date = Column(DateTime)
+    generation = Column(Integer, nullable=False)
+
+    def __init__(self, endorser, proposal):
+        self.endorser = endorser
+        self.proposal = proposal
+        self.generation = proposal.generation
+        self.endorsement_date = datetime.datetime.utcnow()
--- VilfredoReloadedCore/test/test_models.py
+++ VilfredoReloadedCore/test/test_models.py
@@ -2,7 +2,7 @@
 #
 # This file is part of VilfredoReloadedCore.
 #
-# Copyright (c) 2013 Daniele Pizzolli <daniele@ahref.eu>
+# Copyright (c) 2013 Derek Paterson <athens_code@gmx.com>
 #
 # VilfredoReloadedCore is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -36,25 +36,207 @@ from .. import models
 from .. database import db_session, drop_db, init_db
 
 
-class ModelTest(unittest.TestCase):
-    '''
-    Model test
-    '''
-
+class UserTest(unittest.TestCase):
     def setUp(self):
         init_db()
-        pass
 
     def tearDown(self):
         drop_db()
 
-    def test_user_model(self):
-        """Test user model"""
-
-        user = models.LoginUser('test_username', 'test_email', 'test_password')
+    def test_create_user(self):
+        user = models.User('test_username', 'test_email', 'test_password')
         db_session.add(user)
         db_session.commit()
-        user1 = models.LoginUser.query.filter(
-            models.LoginUser.username == 'test_username'
+        user1 = models.User.query.filter(
+            models.User.username == 'test_username'
         ).first()
         self.assertEqual(user.email, user1.email)
+
+
+class QuestionTest(unittest.TestCase):
+    def setUp(self):
+        init_db()
+
+    def tearDown(self):
+        drop_db()
+
+    def test_create_question(self):
+        user = models.User('test_username', 'test_email', 'test_password')
+        db_session.add(user)
+        db_session.commit()
+        question = models.Question(
+            user, 'Question Title',
+            'Question content')
+        db_session.add(question)
+        db_session.commit()
+        question1 = models.Question.query.filter(
+            models.Question.title == 'Question Title'
+        ).first()
+        self.assertEqual(question1.blurb, 'Question content')
+        self.assertEqual(question1.author.username, user.username)
+
+
+class SubscriptionTest(unittest.TestCase):
+    def setUp(self):
+        init_db()
+
+    def tearDown(self):
+        drop_db()
+
+    def test_create_question_subscription(self):
+        user = models.User('test_username', 'test_email', 'test_password')
+        db_session.add(user)
+        db_session.commit()
+        question = models.Question(
+            user, 'Question Title',
+            'Question content')
+        db_session.add(question)
+        db_session.commit()
+
+        user.subscribed_questions.append(models.Update(user, question))
+        db_session.commit()
+
+        update = models.Update.query.filter(
+            models.Update.user_id == user.id
+        ).first()
+        self.assertEqual(update.question_id, question.id)
+
+
+class ProposalTest(unittest.TestCase):
+    def setUp(self):
+        init_db()
+
+    def tearDown(self):
+        drop_db()
+
+    def test_create_proposal(self):
+        user = models.User('test_username', 'test_email', 'test_password')
+        db_session.add(user)
+        db_session.commit()
+
+        question = models.Question(
+            user, 'Question Title',
+            'Question content')
+        db_session.add(question)
+        db_session.commit()
+
+        proposal = models.Proposal(
+            user, question, 'Proposal Title',
+            'Proposal content')
+        db_session.add(proposal)
+        db_session.commit()
+
+        proposal1 = models.Proposal.query.filter(
+            models.Proposal.title == 'Proposal Title'
+        ).first()
+        self.assertEqual(proposal1.blurb, 'Proposal content')
+        self.assertEqual(proposal1.author.username, user.username)
+
+
+class EndorseTest(unittest.TestCase):
+    def setUp(self):
+        init_db()
+
+    def tearDown(self):
+        drop_db()
+
+    def test_endorse_proposals(self):
+        # Create users
+        john = models.User('john', 'john@example.com', 'fgfdfg')
+        susan = models.User('susan', 'susan@example.com', 'bgtyhj')
+        bill = models.User('bill', 'bill@example.com', 'h6ygf')
+
+        db_session.add_all([john, susan, bill])
+        db_session.commit()
+
+        # Create questions
+        johns_q = models.Question(
+            john, 'Johns Question',
+            'What shall we do with the plutonium')
+        johns_q2 = models.Question(
+            john, 'Johns Other Question',
+            'What shall we do with the uranium')
+
+        db_session.add_all([johns_q, johns_q2])
+        db_session.commit()
+
+        # Create invitations (user invites users)
+        john.invite(susan, johns_q.id)
+        john.invite(bill, johns_q.id)
+        john.invite(bill, johns_q2.id)
+        db_session.commit()
+        for inv in john.invites:
+            print "Invitation for question ", inv.question_id,
+            " sent to user ",
+            inv.receiver.username
+
+        print "Bills invitations"
+        for inv in bill.invitations:
+            print inv.question_id, " from author ", inv.sender.username
+
+        # Creating proposals
+        bills_prop1 = models.Proposal(
+            bill, johns_q, 'Bills First Proposal',
+            'Bills blurb of varying interest')
+        bills_prop2 = models.Proposal(
+            bill, johns_q, 'Bills Second Proposal',
+            'Bills blurb of varying disinterest')
+        susans_prop1 = models.Proposal(
+            susan, johns_q, 'Susans Only Proposal',
+            'My blub is cool')
+
+        db_session.add_all([bills_prop1, bills_prop2, susans_prop1])
+        db_session.commit()
+
+        # Subscribing users to questions
+        bill.subscribed_questions.append(models.Update(bill, johns_q))
+        susan.subscribed_questions.append(models.Update(susan, johns_q))
+        db_session.commit()
+
+        # Let the endorsements commence
+        susans_prop1.endorse(susan)
+        susans_prop1.endorse(john)
+        bills_prop1.endorse(john)
+        bills_prop2.endorse(bill)
+        db_session.commit()
+
+        endorsements = models.Endorsement.query.filter(
+            models.Endorsement.proposal_id == susans_prop1.id).count()
+        self.assertEqual(endorsements, 2)
+        endorsements = models.Endorsement.query.filter(
+            models.Endorsement.proposal_id == bills_prop1.id).count()
+        self.assertEqual(endorsements, 1)
+        endorsements = models.Endorsement.query.filter(
+            models.Endorsement.proposal_id == bills_prop2.id).count()
+        self.assertEqual(endorsements, 1)
+
+        susans_prop1_endorsers = set()
+        endorsements = models.Endorsement.query.filter(
+            models.Endorsement.proposal_id == susans_prop1.id)
+        for endorsement in endorsements:
+            susans_prop1_endorsers.add(endorsement.endorser)
+        self.assertTrue(susan in susans_prop1_endorsers)
+        self.assertTrue(john in susans_prop1_endorsers)
+        self.assertFalse(bill in susans_prop1_endorsers)
+
+
+class TestWhoDominatesWho(unittest.TestCase):
+    def setUp(self):
+        init_db()
+
+    def tearDown(self):
+        drop_db()
+
+    def test_proposal_domination(self):
+        p1 = {1, 2, 3}
+        p2 = {2, 3, 4, 5, 6, 7}
+        p3 = {1, 2, 7, 8, 9, 10}
+        p4 = {4, 5, 6}
+        p5 = {4, 5, 6}
+        p6 = {}
+        p7 = {}
+        self.assertEqual(models.Proposal.who_dominates_who(p2, p4), p2)
+        self.assertEqual(models.Proposal.who_dominates_who(p4, p5), -1)
+        self.assertEqual(models.Proposal.who_dominates_who(p2, p3), 0)
+        self.assertEqual(models.Proposal.who_dominates_who(p6, p7), -1)
+        self.assertEqual(models.Proposal.who_dominates_who(p1, p7), p1)
