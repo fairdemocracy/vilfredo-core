58c75b2f833c38e7b8da4b400020be662ad3e07b
Derek Paterson <athens_code@gmx.com>
2013-05-19 11:47:34 +0100

    Made default settings more general.

--- VilfredoReloadedCore/defaults_settings.py
+++ VilfredoReloadedCore/defaults_settings.py
@@ -14,9 +14,9 @@ SECRET_KEY = 'ai4ohngaek4ohchaesheeY2Xee2jishe'
 # mail server settings - ARNNG: must be changed to False in production!
 MAIL_SERVER = 'localhost'
 MAIL_PORT = 25
-MAIL_USERNAME = 'vilfredo'
+MAIL_USERNAME = 'admin'
 MAIL_PASSWORD = None
 MAIL_SUPPRESS_SEND = True
 
 # administrator list
-ADMINS = ['admin@vilfredo-reloaded.org']
+ADMINS = ['admin@example.com']
