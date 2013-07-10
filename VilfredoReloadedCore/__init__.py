800e411d868155ef35e3fc9fc9644bf9e10300c5
Daniele Pizzolli <daniele@ahref.eu>
2013-07-10 12:14:39 +0200

    Do not use docstring as a temporary way to comment the code
    This has the side effect of breaking the documentation.

--- VilfredoReloadedCore/__init__.py
+++ VilfredoReloadedCore/__init__.py
@@ -88,14 +88,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Set logging
 config_file = os.path.join(basedir, app.config['LOG_CONFIG_FILE'])
 
-'''
-# Passing mode='w' to file handler not causing overwrite
-if (os.path.isfile('/var/tmp/vr.log')):
-    try:
-        os.remove('/var/tmp/vr.log')
-    except IOError:
-        print 'Failed to delete log file /var/tmp/vr.log'
-'''
+
+# # Passing mode='w' to file handler not causing overwrite
+# if (os.path.isfile('/var/tmp/vr.log')):
+#     try:
+#         os.remove('/var/tmp/vr.log')
+#     except IOError:
+#         print 'Failed to delete log file /var/tmp/vr.log'
+
 logging.config.fileConfig(config_file)
 # create logger
 logger = logging.getLogger('vilfredo_logger')
