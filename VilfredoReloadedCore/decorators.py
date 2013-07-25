bb9fbd2cd478a1d477ec38af61bdb45754916052
Derek Paterson <athens_code@gmx.com>
2013-07-25 21:22:58 +0100

    Added functools.wraps for decorator.

--- VilfredoReloadedCore/decorators.py
+++ VilfredoReloadedCore/decorators.py
@@ -25,9 +25,11 @@ Decorators
 '''
 
 from threading import Thread
+from functools import wraps
 
 
 def async(f):
+    @wraps(f)
     def wrapper(*args, **kwargs):
         thr = Thread(target=f, args=args, kwargs=kwargs)
         thr.start()
