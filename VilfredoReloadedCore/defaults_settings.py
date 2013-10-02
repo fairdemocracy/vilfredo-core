0ea5e44c51596c7a48e3d0c75b5a296a0738cda1
Derek Paterson <athens_code@gmx.com>
2013-10-02 20:52:34 +0100

    Changed all recently updated files from UTF-16 back to UTF-8 to fix build error. Setup works again and both tests complete successfully. Also added a file header to defaults_settings.py

--- VilfredoReloadedCore/defaults_settings.py
+++ VilfredoReloadedCore/defaults_settings.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of VilfredoReloadedCore.
+#
+# Copyright © 2009-2013 Pietro Speroni di Fenizio / Derek Paterson.
+#
+# VilfredoReloadedCore is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation version 3 of the License.
+#
+# VilfredoReloadedCore is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with VilfredoReloadedCore.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 # default configuration settings
 
 # WARNNG: must be changed in production!
